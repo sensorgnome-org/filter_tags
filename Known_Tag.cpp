9ca2318258e4b6230bd3a0189976a8245ec4bbe8
john brzustowski <jbrzusto@fastmail.fm>
2017-02-22 12:41:56 +0000

    convert frequency to MHz before printing diagnostic

--- Known_Tag.cpp
+++ Known_Tag.cpp
@@ -14,7 +14,7 @@ Known_Tag::Known_Tag(Lotek_Tag_ID lid, string proj, Nominal_Frequency_kHz freq, 
   fid << proj << '#' << std::setprecision(4) << lid << '@' << std::setprecision(6) << freq / 1000.0 << ':' << round(10 * bi) / 10;
   fullID = fid.str();
   if (all_fullIDs.count(fullID)) {
-    std::cerr << "Warning - two very similar tags in project " << proj << ":\nLotek ID: " << lid << "; frequency: " << freq << " MHz; burst interval: " << round(10 * bi) / 10 << " sec\nAppending '!' to fullID of second one.\n";
+    std::cerr << "Warning - two very similar tags in project " << proj << ":\nLotek ID: " << lid << "; frequency: " << (freq / 1000.0) << " MHz; burst interval: " << round(10 * bi) / 10 << " sec\nAppending '!' to fullID of second one.\n";
     while (all_fullIDs.count(fullID)) {
       fullID += '!';
     };
@@ -22,5 +22,5 @@ Known_Tag::Known_Tag(Lotek_Tag_ID lid, string proj, Nominal_Frequency_kHz freq, 
   all_fullIDs.insert(fullID);
 };
 
-std::unordered_set < std::string > 
+std::unordered_set < std::string >
 Known_Tag::all_fullIDs;       // will be populated as tags database is built
